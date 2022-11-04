63bc92f53efb2b5e50814d9756d75d3c5521fd92
ngljump04 <b2741738@ben.edu>
2022-11-04 18:32:35 -0500

    Second Commit

---
+++ NigelWhitaker_2200_PE6.13/src/Exercise06_13.java
@@ -0,0 +1,30 @@
+/**************************************************************
+ * 
+ * @author Nigel Whitaker
+ * Date: 11/04/2022
+ *
+ *************************************************************/
+public class Exercise06_13 {
+
+	public static void main(String[] args) {
+
+		double seriesStart  =  1.0;	
+		double numberOfRows = 20.0;	
+
+		System.out.println("\ni         m(i)     ");
+		System.out.println("-------------------");
+
+		for (double i = seriesStart; i <= numberOfRows; i++) {
+			System.out.printf("%-10.0f", i);
+			System.out.printf("%7.4f\n", sumSeries(i));
+		}
+	}
+
+	public static double sumSeries(double n) {
+		double sum = 0;	
+		for (double i = 1; i <= n; i++) {
+			sum += i / (i + 1);
+		}
+		return sum;
+	}
+}
